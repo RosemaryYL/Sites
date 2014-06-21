972bc87c8a5ff25e78c9e932fbd06dc31e515cce
杨砾 <lizzyyang@anjuke.com>
2014-06-21 17:58:37 +0800

    update

--- index.php
+++ index.php
@@ -13,37 +13,37 @@
 	<?php
 		require_once("config.php");
 		$config_map = array(
-			'Black Citadel' => '',
-			'Blazeridge Steppes' => '',
-			'Bloodtide Coast' => '',
-			'Brisban Wildlands' => '',
-			'Caledon Forest' => '',
-			'Cursed Shore' => '',
-			'Diessa Plateau' => '',
-			'Divinity\'s Reach' => '',
-			'Dredgehaunt Cliffs' => '',
-			'Fields of Ruin' => '',
-			'Fireheart Rise' => '',
-			'Frostgorge Sound' => '',
-			'Gendarran Fields' => '',
-			'Harathi Hinterlands' => '',
-			'Hoelbrak' => '',
-			'Iron Marches' => '',
-			'Kessex Hills' => '',
-			'Lornar\'s Pass' => '',
-			'Malchor\'s Leap' => '',
-			'Metrica Province' => '',
-			'Mount Maelstrom' => '',
-			'Plains of Ashford' => '',
-			'Queensdale' => '',
-			'Rata Sum' => '',
-			'Snowden Drifts' => '',
-			'Southsun Cove' => '',
-			'Sparkfly Fen' => '',
-			'Straits of Devastation' => '',
-			'The Grove' => '',
-			'Timberline Falls' => '',
-			'Wayfarer Foothills' => ''
+			'Black Citadel' => '黑烟壁垒',
+			'Blazeridge Steppes' => '裂脊山脉',
+			'Bloodtide Coast' => '血潮海岸',
+			'Brisban Wildlands' => '布里斯班野地',
+			'Caledon Forest' => '卡勒顿之森',
+			'Cursed Shore' => '诅咒海岸',
+			'Diessa Plateau' => '底耶沙高地',
+			'Divinity\'s Reach' => '神佑之城',
+			'Dredgehaunt Cliffs' => '掘洞悬崖',
+			'Fields of Ruin' => '废墟原野',
+			'Fireheart Rise' => '炎心高地',
+			'Frostgorge Sound' => '霜谷之音',
+			'Gendarran Fields' => '甘达拉战区',
+			'Harathi Hinterlands' => '哈希拉腹地',
+			'Hoelbrak' => '霍布雷克',
+			'Iron Marches' => '钢铁平原',
+			'Kessex Hills' => '凯西斯山',
+			'Lornar\'s Pass' => '罗纳通道',
+			'Malchor\'s Leap' => '马尔科之跃',
+			'Metrica Province' => '度量领域',
+			'Mount Maelstrom' => '旋涡山',
+			'Plains of Ashford' => '阿什福德平原',
+			'Queensdale' => '女王谷',
+			'Rata Sum' => '拉塔索姆',
+			'Snowden Drifts' => '漂流雪径',
+			'Southsun Cove' => '南阳海湾',
+			'Sparkfly Fen' => '闪萤沼泽',
+			'Straits of Devastation' => '浩劫海峡',
+			'The Grove' => '圣林之地',
+			'Timberline Falls' => '林线瀑布',
+			'Wayfarer Foothills' => '旅者丘陵'
 		);
 		if (isset($_GET['map'])) {
 			$maps = explode(',', $_GET['map']);
@@ -64,7 +64,7 @@
 			}
 	?>
 	<tr<?=$style;?>>
-		<td><?=$row['cnname'];?></td><td><?=$row['name'];?></td><td><img width="360px" src="http://106.186.18.29/img/<?=$row['pic_url'];?>"></td><td><?=$row['map'];?></td><td><?=$row['way_point'];?></td><td><?=$row['code'];?></td>
+		<td><?=$row['cnname'];?></td><td><?=$row['name'];?></td><td><img width="360px" src="http://106.186.18.29/img/<?=$row['pic_url'];?>"></td><td><?=$row['map'];?></td><td><?=$config_map[$row['way_point']];?></td><td><?=$row['code'];?></td>
 	</tr>
 	<?php
 		}
