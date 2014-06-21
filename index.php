a0bcab0be4803f2979aacd83fef219ca266dfa67
杨砾 <lizzyyang@anjuke.com>
2014-06-21 17:22:26 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php echo "aaa";?>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
 
@@ -13,9 +12,46 @@
 	</tr>
 	<?php
 		require_once("config.php");
+		$config_map = array(
+			'Black Citadel' => '',
+			'Blazeridge Steppes' => '',
+			'Bloodtide Coast' => '',
+			'Brisban Wildlands' => '',
+			'Caledon Forest' => '',
+			'Cursed Shore' => '',
+			'Diessa Plateau' => '',
+			'Divinity\'s Reach' => '',
+			'Dredgehaunt Cliffs' => '',
+			'Fields of Ruin' => '',
+			'Fireheart Rise' => '',
+			'Frostgorge Sound' => '',
+			'Gendarran Fields' => '',
+			'Harathi Hinterlands' => '',
+			'Hoelbrak' => '',
+			'Iron Marches' => '',
+			'Kessex Hills' => '',
+			'Lornar\'s Pass' => '',
+			'Malchor\'s Leap' => '',
+			'Metrica Province' => '',
+			'Mount Maelstrom' => '',
+			'Plains of Ashford' => '',
+			'Queensdale' => '',
+			'Rata Sum' => '',
+			'Snowden Drifts' => '',
+			'Southsun Cove' => '',
+			'Sparkfly Fen' => '',
+			'Straits of Devastation' => '',
+			'The Grove' => '',
+			'Timberline Falls' => '',
+			'Wayfarer Foothills' => ''
+		);
+		$maps = explode(',', $_GET['map']);
 		$arrRows = $config;
 		$i = 0;
 		foreach ($arrRows as $row) {
+			if ($maps && !in_array($row['map'], $maps)) {
+				continue;
+			}
 			$i++;
 			if ($i%2 == 0) {
 				$style = ' style="background-color: #fff8e8;"';
