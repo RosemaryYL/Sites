175e98901caa0ff318efa8ae0d5280609f88e82c
杨砾 <lizzyyang@anjuke.com>
2014-06-21 16:15:17 +0800

    update

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <?php echo "aaa";?>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+
+  <meta charset="utf-8">
+ </head>
+
+ <body class="docs ">
+ 	<table class="pve center table" style="width:auto">
+<tbody>
+	<tr style="vertical-align:bottom;">
+	<th> cnname </th> <th> name</th><th> pic </th><th> map </th><th>way_point</th>
+	</tr>
+	<?php
+	require_once("config.php");
+		$arrRows = $config;
+		foreach ($arrRows as $row) {
+	?>
+	<tr>
+		<td><?=$row['cnname'];?></td><td><?=$row['name'];?></td><td><img scr="/img/<?=$row['pic_url'];?>"></td><td><?=$row['map'];?></td><td><?=$row['way_point'];?></td>
+	</tr>
+	<?php
+		}
+	?>
+</tbody>
+</table>
+ </body>
+ </html>
